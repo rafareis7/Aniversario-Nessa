a9ec9ee6f070bc24d2e6b19d94ac162c29c518b3
rafareis7 <84430982+rafareis7@users.noreply.github.com>
2022-04-23 12:50:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,173 @@
+var listaNaruto = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuFWe0nlezSpcBw7j8e51YYE7rvj2Ynnxirg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReOuDVizLjh4dPFNOS32L2myt9ClqqezA4VQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSubOhvXJ6pFbGSqpAhupVnbf3Fa_r2lY949A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLwso48WgRPxGufw1FI3fnsNZCyavTRtOs6A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGMrt6rRSl3rjKK_lbq0x3z7KnAzB1qhymKA&usqp=CAU",
+  "https://s2.glbimg.com/1MADinWWD66trpq1ZcA8FuNW8Bw=/1080x608/top/smart/https://i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/o/l/fHKsV4QDqIvBzVE2HJPg/naruto-fortnite.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-zBPJ_ZbROqUmC8r22EV3C0tRcaJhiHZ4Qw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREe4YrWpIvDMgTnum7IyxnWanyW9aHvLqi4g&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQok6YvSrS6zMPE-KwOVmCcAUJDj8c5S-8jog&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY_ILWaW6h11DItKRGisj6wDI4b0IrXIlWbw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoUS9Y3rsLsrZMm0ETylKcUScZQG96M0aRIw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHFlPOQBiGKvNKBYYOtgYjZNLNp0m4wUQw7g&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE1AudGwwa9foK_ajhoKCnffSbO6WJQFMj-g&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgpI_qzC11mGJJPtl3LTPzi2DSUeHf5tnygQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSipKxtd0u4QqB28C3JBZXAp1SNA8Kf1zZx3g&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbtnSBZJJ1oFxc23fTB13PRX7wwNP1iwBBTw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAPDstxgU_X1gWB7QX_yZ6-c7Ytxvz5m3kLQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcUfj_PRd1Zb_eUqThXPSPVB7uyMJOTeALQw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFRqYzz5ADnsA-y_8rzqS9Mh7LYOs-r_jnYA&usqp=CAU",
+
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1Xy_CGadYqh7oYG_dKROTPBPuYmToaNj7Bg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ93rZawQ4Ol0C0hjvtURFjhL7-1DHFZXXh-A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_3k-_taMIZwdhUnQ-lmTE7tFYAmCQ8p4GIA&usqp=CAU"
+];
+
+document.write("<h2> Narutinho </h2>");
+
+for (var i = 0; i < listaNaruto.length; i++) {
+  document.write(
+    "<a href= " + listaNaruto[i] + ">" + "<img src= " + listaNaruto[i] + "></a>"
+  );
+}
+
+var listaKuroko = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXpbPxd-px9t_iN34d-dzlBMuQP5sKH6Lq3A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD9zt7200k-VlcV9H0niz8BknXa8_cVKmQew&usqp=CAU"
+];
+
+document.write("<h2> Kurokinho </h2>");
+
+listaKuroko.push(
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWMeHAp_HiO-2Qs1lsk89OL07LNxPBOd0LRQ&usqp=CAU",
+  "https://i.pinimg.com/736x/1b/86/82/1b86826d9fde060ff183e942cb6e2594--season--hot-anime.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHsBzT-d_E2zCD0DWqCWYpohBN9eJQn4YhFA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP98zy8m-imopc_H2XgqeIMUx7fJjQaVBrRA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8KhAsC4ZWkrg4gDBoCtCspYLwZsWfAAwz7w&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg1nCjQR3B9FobfCyK0gDmuYpKWvosxbUirA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgW84Y5kfqBtzav2ccbMcMsOiYo4fSLvDwvw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRudDS4B-o4Y9Yz7BIuIG58_Pvn-xCvj5m1lw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9G06B0RpPMAayRZ2153FQ3xQVLADyTyRneA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUnBEQjKKwH7MZEDJke4YUW8j9uJJRbEHkzg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS3f8vMMcwJwNwMskaQpDrV-DFD4SO0cWIaQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNy8hK-qDTbVKbDvnbg3rb4fZZfjsxGjvXeg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjziYs-9rXmnUj-4JH6v1omt6OKi0-5P4tHw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0QO5YE7vn7n-7HLTI020pKL6rt9yP20wB0g&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfnULqgGDEr3eg9ssvfjcjNmw2FhloKII5eA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgzPjOArByhhxPPNR39VbMQTPnnZJx-LjaiA&usqp=CAU0",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwB3zqeIH7MOmM-88_knUtrF2R6_FpLgmLjQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbUP_QT1DFkNiHtfcOIW_0XJ65CyJmMNJnrA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQZ3Aj_afZvN89szlk2uXCLHd2nju3R589Zg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY0T_b2s8sy7H9GSHlGbJ44CWsbNbltj7oRA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQT64JmqWgFLdgEU6MXm4k8UKIUP5LxB3XRg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnJLmFEIgVlW-IofEcQmzpTxzufWIqUZHOug&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9haLgtfUFa4nKKdcpJ_gmi1SOFv662-J31Q&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT64TsLUvt9-urx09pMyeonJ_caGHfGvHvohw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGuVtjKcCbZ8VaZvbDfr4gZLFZnVDS6j5utg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYPCizRwGFiKFJsVoWE9yIxIjZDxleSiwr-A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLkh8mOqRRlVnBviJmAZbOsOhwuCEgzDy7Iw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ8ywzcD7UGKUo2LY7B2M0lfF6obOZI4DyCQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMXrk81cYR4r8mqg1q7BWtKaejw16eeqWVjw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSArpRZU6PhGynwm4oJt2NtU1LbiK8cvPfRnQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuxx-OnnQORUdRDpYmG0BFOw8E4Nu6eGbUyQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpW7-OnsbcJP_vQqyRLA3cXg9Yf93nI11yvQ&usqp=CAU"
+);
+
+for (var i = 0; i < listaKuroko.length; i++) {
+  document.write(
+    "<a href= " + listaKuroko[i] + ">" + "<img src= " + listaKuroko[i] + "></a>"
+  );
+}
+
+var listaHaikyuu = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTraomFjaIwMHq6pU-X5bkNyl4Q_5FoFpfeNQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY4QP453LiQj8Wmfmsfkt90Bxon5YiAxEEiw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_EQC7lL1b1V8nfAqyFX-JPUGHKsNipbUPJg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7SYHO_pPo_Xv2R9aZklfZj7DwUrNRtoodmQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqzyCpkDGFIW6xVUk7LUFWQ0lsikRin59UHQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV4abQDlGhJO9wGNatzone2uX7T7wksIriPQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh6uBbfqK9Rj2kCovHqxOXFVi2uYl_Nd1ffw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8QwvkBdS89WfRyfq_fuw814IblO4DteKX5A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBzaglzGVuSgDsNR_QPzuq8PXxtOapCtlXFw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBsUcsmRsTlB1LFORmkQZg11EPhma0lPpHfg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPb5dxYPWbvfzb6-iLJh8XrtkBvkAtzoC26A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfBPyS6xsd-d1Bp3BezJh8PNhDF67A1YdVuQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnk1_bUrXgX6ThrEd29QDjatbouJ-2E7myNg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS4ezR3XzCJIhLrbNDojv59SU9-mQlnBJeZA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc4Z50LMwHx_MOWYzF3OWoiVO30B3_fYiJKQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf-2ZIyJBCHbnV8I4Loiq6GUYoCZHVAaRb9A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhdqc5453ETuEK6XyWloVQNccPhgO-48Y_Cg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRqnX7hxT0e076WiczenL00zlJ7mIrTP2eVA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-DiEutu06nQrhzvk4Pn2SkrZWsoqs1z7bNQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL0N6F5NaLgbivJbofBnfxssQJDbv6dQyFyg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-5Yc1I86K1PoM3Ddx6zQcj8UbYPm4KZPmMQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9oXNZ4goecNqXzQJ22wnqUCXUoTdloCEeKQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9X8jFlOyn5GIvl2hCKDOrF7L1iOaqZtMLiA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI-PYfjupohIxvXw2HnbkOEG3KxN21KuAr8w&usqp=CAU"
+];
+
+document.write("<h2> Haikyuu!!! </h2>");
+
+for (var i = 0; i < listaHaikyuu.length; i++) {
+  document.write(
+    "<a href= " +
+      listaHaikyuu[i] +
+      ">" +
+      "<img src= " +
+      listaHaikyuu[i] +
+      "></a>"
+  );
+}
+
+document.write("<h2> Shingeki no Kyojin - Agredindo o seu Coração </h2>");
+
+var listaKyojin = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQukU03CR-3jFDrq7yEPsfQKO-nMlSL8XmRGQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_bjkCSCMLtUOGP3u7A7azbyulh6k2nn_Igg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSneqiOjb2Vt3rTuvj-BTv6JaLtxUBYWJ3X3g&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj4Vg5zwSMaQTEASCtuzO0GavlUN_DXD1D1A&usqp=CAU",
+  "https://criticalhits.com.br/wp-content/uploads/2021/08/hange-zoe-attack-on-titan-1-1200x675-1-910x512.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIKPMsSA4zjIo93AyBWELZdqwNbYzRt7bViA&usqp=CAU",
+
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeJEGIipMIXPinopTv4QEMqkK2X-2nqW_Afg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTRuV4HXkSze6csnRMUuG-Ke5vtcaO6Y7O0A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs42fxzF2SRfqElOSnahr5K0aBmUq3LVNW8w&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDCN4cYxJ7BUj1MzLUEzVjFuUgcgzT2GGRhw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgxmLy4zG2HP3_gyJVyST-Iao0shZKaso-IQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxXt9Sxzu7VmDWNcbfFtXVrvtqKOZOPvJ-Jw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfB3mY3uBcjbeLOob0ctYUOHr-QzdEzwDf-g&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwsX2CbRimJOj5_J8e9ziNHgXN_MHK5MH1fg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6EBDv3YToD4XRLUYWvVSBrqak2J-7KrUsDQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT51FCkAjgep_VLruPkGa_e5ySND7zKU_g5oQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9lJ2Cirl4AwGLdR49tFVaYVXozqLdm-XOMA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxjS58VY3CZ82vM3x5FRnJYxHGST2xxSB9dQ&usqp=CAU"
+];
+
+for (var i = 0; i < listaKyojin.length; i++) {
+  document.write(
+    "<a href= " + listaKyojin[i] + ">" + "<img src= " + listaKyojin[i] + "></a>"
+  );
+}
+
+document.write("<h2> Mitsuhaaaa! Taki kuuuuun! </h2>");
+
+var listaYn = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLjlp5uucWj0PV8jtuZXwjQyXL_tOn6rT6Cw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQBf3eJo0slhZlkjDfFRbIqs1_6tQ3oHcB2A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXs9Vl-_FshMA7ZA2fkLpAM1HEOO0HIey6Xw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs2RqGTg-NOJyxHny0lg7qgpwLBVPiZP5Gfw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5FVDilxeNO_5JGaHp5FyV5_Ew4Lr1RMVvnA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlJJQoqnCAjM_FHIOfTpt_fHkrEy5Z9X7YKQ&usqp=CAU"
+];
+
+for (var i = 0; i < listaYn.length; i++) {
+  document.write(
+    "<a href= " + listaYn[i] + ">" + "<img src= " + listaYn[i] + "></a>"
+  );
+}
+
+document.write("<br><br> <h2> Obrigado por tudo nê! </h2>");
+document.write(
+  "<h3> Por você eu viro Hokage, a sua sombra, pulo o mais alto possível, agrido, digo, Enrolo seu cachecol e... te salvo de um meteoro? <br> Enfim hahaha, te amo, bjs 😘 </h3>"
+);
